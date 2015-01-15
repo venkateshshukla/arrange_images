b421b6ec02b0e54f773f1b494e3c48bd5d0a23f2
Venkatesh Shukla <venkatesh.shukla.eee11@iitbhu.ac.in>
2015-01-15 10:50:16 -0500

    Find the timestamp of the image given
    Use python imaging library for this and should add it as a dependency.
    The timestamp is extracted from the exif of the images.
    
    Signed-off-by: Venkatesh Shukla <venkatesh.shukla.eee11@iitbhu.ac.in>

--- clean_library.py
+++ clean_library.py
@@ -2,3 +2,32 @@
 # per their time of capture. This would help easing their uploading to google
 # plus photos, making a clean ordered library along the way.
 
+from PIL import Image
+from PIL.ExifTags import TAGS
+from datetime import datetime
+
+def get_exif_dict(name):
+	exif = {}
+	img = Image.open(name)
+	info = img._getexif()
+	for tag, value in info.items():
+		decoded = TAGS.get(tag, tag)
+		exif[decoded] = value
+	return exif
+
+def get_time_exif(name):
+	exif = get_exif_dict(name)
+	time = exif['DateTimeOriginal']
+	return time
+
+def get_timestamp_exif(name):
+	time = get_time_exif(name)
+	date = datetime.strptime(time, "%Y:%m:%d %H:%M:%S")
+	return int(date.strftime('%s'))
+
+name = "sample_img.jpg"
+date = get_timestamp_exif(name)
+print date
+
+
+
