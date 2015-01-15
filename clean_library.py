a326da5568812d867874efc1fa01a5c9ca4bd123
Venkatesh Shukla <venkatesh.shukla.eee11@iitbhu.ac.in>
2015-01-15 00:35:58 -0500

    Initiating
    Added the mission statement to the script.
    
    Signed-off-by: Venkatesh Shukla <venkatesh.shukla.eee11@iitbhu.ac.in>

---
+++ clean_library.py
@@ -0,0 +1,4 @@
+# A small script to arrange the images in the present folders and subfolders as
+# per their time of capture. This would help easing their uploading to google
+# plus photos, making a clean ordered library along the way.
+
