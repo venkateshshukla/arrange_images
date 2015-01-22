ebcfab4c971d143c69a96cbe639cbede22ee80f9
Venkatesh Shukla <venkatesh.shukla.eee11@iitbhu.ac.in>
2015-01-22 23:12:08 +0530

    Better naming, more assertions and tests
    Removed some minor mistakes while performing the above mentioned tasks.
    
    Signed-off-by: Venkatesh Shukla <venkatesh.shukla.eee11@iitbhu.ac.in>

--- img_ops.py
+++ img_ops.py
@@ -6,6 +6,14 @@ import os
 import shutil
 from img_iterator import is_jpeg
 
+def get_filename(path):
+	'''
+	Give the filename of provided path to a file. Use python os.path.split
+	:param path: path of the file whose name is to be returned
+	'''
+	src_path, fl_name = os.path.split(path)
+	return fl_name
+
 def is_present(name, dest):
 	'''
 	Check if a file of given name (included as path) is present in the given
@@ -13,29 +21,36 @@ def is_present(name, dest):
 	:param name: name of the file (may contain whole path. last term is considered)
 	:param dest: destination folder where check is to be performed.
 	'''
-	src_path, img_name = os.path.split(name)
+	img_name = get_filename(name)
 	filename = os.path.join(dest, img_name)
 	return os.path.isfile(filename)
 
-def img_copy(src, dest = "out/", dupl = "out/duplicates/"):
+def img_copy(src = None, dest = None, dest_path = "out/", dupl_path = "out/duplicates/"):
 	'''
 	Copies img from src to dest putting duplicates in dupl.
+	All paths are relative to path of the file.
 	:param src: path to image file.
-	:param dest: destination folder where the file should be put.
-	:param dupl: folder where duplicates, if found, may be put.
+	:param dest: name of the final image file without path. By default src.
+	:param dest_path: destination folder where the file should be put.
+	:param dupl_path: folder where duplicates, if found, may be put.
 	'''
 
 	# Sanity checks
 	assert src is not None, "src path cannot be None"
-	assert dest is not None, "dest path cannot be None"
-	assert dupl is not None, "dupl path cannot be None"
+	assert dest is not None, "dest name cannot be None"
+	assert dest_path is not None, "dest path cannot be None"
+	assert dupl_path is not None, "dupl path cannot be None"
 	assert os.path.isfile(src), "src is not present"
 	assert is_jpeg(src), "src is not a jpeg"
+	assert is_jpeg(dest), "dest is not a jpeg"
+
+	src_filename = get_filename(src)
+	dest_filename = get_filename(dest)
 
 	path = os.path.abspath(os.path.dirname(__file__))
 
-	dest_dir = os.path.join(path, dest)
-	dupl_dir = os.path.join(path, dupl)
+	dest_dir = os.path.join(path, dest_path)
+	dupl_dir = os.path.join(path, dupl_path)
 	ddupl_dir = os.path.join(dupl_dir, 'duplicates')
 
 	if not os.path.isdir(dest_dir):
@@ -44,7 +59,8 @@ def img_copy(src, dest = "out/", dupl = "out/duplicates/"):
 		os.makedirs(dupl_dir)
 
 	if is_present(src, dest_dir):
-		img_copy(src, dest = dupl_dir, dupl = ddupl_dir)
+		img_copy(src, dest, dest_path = dupl_dir, dupl_path = ddupl_dir)
 	else:
-		shutil.copy(src, dest_dir)
+		final_path = os.path.join(dest_dir, dest_filename)
+		shutil.copy(src, final_path)
 
--- test_img_ops.py
+++ test_img_ops.py
@@ -11,6 +11,12 @@ null_img = None
 # Non existent img
 ne_img = "nonexistent.jpg"
 
+# Non jpeg file
+nj_file = "nonjpg"
+
+# Sample destination file
+dest_img = "sample_destn.jpg"
+
 #Test counter
 i = 0
 
@@ -36,6 +42,51 @@ except AssertionError as e:
 i += 1
 try:
 	img_copy(ne_img)
+except AssertionError as e:
+	print "Test %d raised assertion." % i,
+	print e
+	pass
+
+# Test 4
+i += 1
+try:
+	img_copy(nj_file)
+except AssertionError as e:
+	print "Test %d raised assertion." % i,
+	print e
+	pass
+
+# Test 5
+i += 1
+try:
+	img_copy(sample_img, dest_img)
+except AssertionError as e:
+	print "Test %d raised assertion." % i,
+	print e
+	pass
+
+# Test 6
+i += 1
+try:
+	img_copy(sample_img, null_img)
+except AssertionError as e:
+	print "Test %d raised assertion." % i,
+	print e
+	pass
+
+# Test 7
+i += 1
+try:
+	img_copy(sample_img, ne_img)
+except AssertionError as e:
+	print "Test %d raised assertion." % i,
+	print e
+	pass
+
+# Test 8
+i += 1
+try:
+	img_copy(sample_img, nj_file)
 except AssertionError as e:
 	print "Test %d raised assertion." % i,
 	print e
