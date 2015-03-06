28232121354de2c2d87ecc9151f1989c504d0946
Venkatesh Shukla <venkatesh.shukla.eee11@iitbhu.ac.in>
2015-03-06 21:17:36 +0530

    Made an executable of clean_library
    Change mode of the file and included the standard hash-bang line for python.
    
    Signed-off-by: Venkatesh Shukla <venkatesh.shukla.eee11@iitbhu.ac.in>

--- clean_library.py
+++ clean_library.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # A small script to arrange the images in the present folders and subfolders as
 # per their time of capture. This would help easing their uploading to google
 # plus photos, making a clean ordered library along the way.
